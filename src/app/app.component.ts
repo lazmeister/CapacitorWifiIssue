3578de2652e3e9a83393c7096db1e0ab436930ab
Laszlo Alt <laszlo.alt@gmail.com>
2023-02-07 20:10:59 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Network } from '@capacitor/network';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+export class AppComponent implements OnInit {
+
+  currentNetworkStatus: boolean = false;
+  networkConnectionWifi: boolean = false;
+
   constructor() {}
+
+  ngOnInit(): void {
+
+    Network.getStatus().then(currentStatus =>{
+      this.currentNetworkStatus = currentStatus.connected;
+      if (currentStatus.connectionType === 'wifi') {
+        // Only start syncing pictures if the user wants wifi syncing and the network connection is wifi
+        this.networkConnectionWifi = true;
+      } else {
+        this.networkConnectionWifi = false;
+      }
+      console.log(currentStatus.connected, currentStatus.connectionType)
+    });
+    // Listen to network changes
+    Network.addListener('networkStatusChange', status => {
+      if (status.connectionType === 'wifi') {
+        // Only start syncing pictures if the user wants wifi syncing and the network connection is wifi
+        this.networkConnectionWifi = true;
+        console.log('wifi is on',status.connectionType);
+      } else {
+        this.networkConnectionWifi = false;
+        console.log('wifi is off',status.connectionType);
+      }
+      // Check connection status
+      this.currentNetworkStatus = status.connected;
+      console.log(status.connected, status.connectionType)
+    });
+  }
 }
